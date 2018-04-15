f1ddd0cf0853a426ff68996be04b11b6339ccae5
SBT <sbt@Antons-MacBook-Pro-2.local>
2018-04-15 19:44:47 +0300

    initial

---
+++ src/main/java/com/juhnowski/Main.java
@@ -0,0 +1,43 @@
+package com.juhnowski;
+
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.io.RandomAccessFile;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import java.io.File;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        PDFParser parser = null;
+        PDDocument pdDoc = null;
+        COSDocument cosDoc = null;
+        PDFTextStripper pdfStripper;
+
+        String parsedText;
+        String fileName = "/Users/sbt/Downloads/otvety.pdf";
+        File file = new File(fileName);
+        try {
+            parser = new PDFParser(new RandomAccessFile(file,"r"));
+            parser.parse();
+            cosDoc = parser.getDocument();
+            pdfStripper = new PDFTextStripper();
+            pdDoc = new PDDocument(cosDoc);
+            parsedText = pdfStripper.getText(pdDoc);
+            System.out.println(parsedText.intern());
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                if (cosDoc != null)
+                    cosDoc.close();
+                if (pdDoc != null)
+                    pdDoc.close();
+            } catch (Exception e1) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
